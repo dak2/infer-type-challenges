0680f8457c86a84a683e7e0d9c58f89be605865c
dak2 <dawt2h@gmail.com>
2024-10-17 00:14:48 +0000

    add google login endpoint

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -2,7 +2,6 @@ import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const users = sqliteTable("users", {
   id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+  googleUserId: text("google_user_id"), // NOTE: This column is nullable because another oauth flow might be used.
   name: text("name").notNull(),
-  email: text("email").notNull(),
-  password: text("password").notNull(),
 });
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,42 @@
+import { drizzle } from "drizzle-orm/d1"
 import { Hono } from 'hono'
+import { googleAuth } from '@hono/oauth-providers/google'
+import { users } from "./db/schema"
 
-const app = new Hono()
+type Bindings = {
+  DB: D1Database;
+}
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+const app = new Hono<{ Bindings: Bindings }>()
+
+app.get('/', async (c) => {
+  const db = drizzle(c.env.DB)
+  const result = await db.select().from(users).all()
+  return c.text(`${result[0].googleUserId}`)
 })
+
+app.get(
+  '/google-login',
+  googleAuth({
+    scope: ['profile'],
+  }),
+  async (c) => {
+    const user = c.get('user-google')
+    if (!user) {
+      return c.json({ error: 'Unauthorized' }, 401)
+    }
+    if (!user.id) {
+      return c.json({ error: 'Unauthorized' }, 401)
+    }
+
+    const db = drizzle(c.env.DB)
+    await db.insert(users).values({
+      googleUserId: user.id,
+      name: user.name || '',
+    });
+
+    return c.text('Logged in')
+  }
+)
 
 export default app
